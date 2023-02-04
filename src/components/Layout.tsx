84dfff08a05597145f8803011699a2a0b91babd5
Sam Gibbs <sam-gibbs1@hotmail.co.uk>
2023-02-04 20:11:02 +0000

    script fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,6 +12,7 @@ export default function Layout({ children }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="apple-touch-icon" href="/icon.png" />
+        <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         <meta name="theme-color" content="#fff" />
       </Head>
       <nav>
